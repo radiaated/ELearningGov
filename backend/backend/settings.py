2003b8b7723c12c56c67907c4f7e65d94801ffea
rad <iamsumankht@gmail.com>
2023-07-14 16:19:32 +0545

    'Proto'

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,9 +140,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'dbmain_nt1d',
+
+        'USER': 'dbmain',
+
+        'PASSWORD': 'gjEpemgZ2LTCSqC00OLvAkZbF02DaATo',
+
+        'HOST': 'dpg-ciofdotgkuvh5gnj7l7g-a.singapore-postgres.render.com',
+
+        'PORT': '5432',
     }
 }
 
