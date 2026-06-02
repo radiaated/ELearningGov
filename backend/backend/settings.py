0d4ec8adb895bcd9413b30bba9368620e4dc2379
radiaated <iamsumankht@gmail.com>
2026-06-02 21:51:56 +0545

    refactor: change local db config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,7 +146,7 @@ DATABASES = {
     }
     # "default": {
     #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db1.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
     # }
 }
 
